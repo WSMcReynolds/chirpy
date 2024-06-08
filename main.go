f9b403bb091a0f0c70bdcb03d409cecec05f997d
William McReynolds <w.s.mcreynolds@gmail.com>
2024-06-08 12:01:52 -0500

    Add health check endpoint

--- main.go
+++ main.go
@@ -5,12 +5,19 @@ import (
 	"net/http"
 )
 
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	port := "8080"
 	filepathRoot := "."
 	sm := http.NewServeMux()
 
-	sm.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	sm.HandleFunc("/healthz", healthCheckHandler)
+	sm.Handle("/app/*", http.FileServer(http.Dir(filepathRoot)))
 
 	server := &http.Server{
 		Addr:    ":" + port,
