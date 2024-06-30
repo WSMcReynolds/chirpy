0bc7a36f1127b7c2495a0c2c346441de6c18bc65
William McReynolds <w.s.mcreynolds@gmail.com>
2024-06-30 16:47:02 -0500

    Add chirp retrieval by author id

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -30,20 +30,44 @@ func (cfg *apiConfig) chirpsGetHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) chirpsRetrieveHandler(w http.ResponseWriter, r *http.Request) {
+
 	dbChirps, err := cfg.DB.GetChirps()
+
+	chirps := []Chirp{}
+	authorId := 0
+
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chrips")
 		return
 	}
 
-	chirps := []Chirp{}
+	authorIdString := r.URL.Query().Get("author_id")
+	if authorIdString != "" {
+		authorId, err = strconv.Atoi(authorIdString)
+	}
 
-	for _, chirp := range dbChirps {
-		chirps = append(chirps, Chirp{
-			Id:       chirp.Id,
-			AuthorId: chirp.AuthorId,
-			Body:     chirp.Body,
-		})
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "invalid author id")
+	}
+
+	if authorId > 0 {
+		for _, chirp := range dbChirps {
+			if chirp.AuthorId == authorId {
+				chirps = append(chirps, Chirp{
+					Id:       chirp.Id,
+					AuthorId: chirp.AuthorId,
+					Body:     chirp.Body,
+				})
+			}
+		}
+	} else {
+		for _, chirp := range dbChirps {
+			chirps = append(chirps, Chirp{
+				Id:       chirp.Id,
+				AuthorId: chirp.AuthorId,
+				Body:     chirp.Body,
+			})
+		}
 	}
 
 	sort.Slice(chirps, func(i, j int) bool {
